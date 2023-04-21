18045c77f663209ac9332753592255748420e1a5
Jesper Brunnstroem <jbrunnst@esat.kuleuven.ac.be>
2023-04-21 16:07:32 +0200

    changed setup.py to install package as aspcore

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="aspcore-PVJesper",
+    name="aspcore",
     version="0.0.1",
     author="Jesper Brunnström",
     author_email="author@example.com",
