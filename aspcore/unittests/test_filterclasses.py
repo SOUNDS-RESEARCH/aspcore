0be6d765f3b53e3109fb8a7be98d2fd82b3f9a25
Jesper Brunnstroem <jbrunnst@esat.kuleuven.ac.be>
2023-05-13 01:54:20 +0200

    added tests and fixed code for basic FilterSumDynamic

--- aspcore/unittests/test_filterclasses.py
+++ aspcore/unittests/test_filterclasses.py
@@ -60,48 +60,66 @@ def test_filtersum_dynamic_with_static_ir_equals_normal_filtersum(ir_len, num_in
     for i in range(num_samples):
         out[:,i:i+1] = filt.process(sig[:,i:i+1])
         out_dyn[:,i:i+1] = filt_dyn.process(sig[:,i:i+1])
-    # for i in range(num_blocks):
-    #     out[:, i * ir_len : (i + 1) * ir_len] = filt.process(
-    #         sig[:, i * ir_len : (i + 1) * ir_len]
-    #     )
-    #     out_dyn[:, i * ir_len : (i + 1) * ir_len] = filt_dyn.process(
-    #         sig[:, i * ir_len : (i + 1) * ir_len]
-    #     )
-
-
     assert np.allclose(out, out_dyn)
+
+
+@hyp.settings(deadline=None)
+@hyp.given(
+    num_in = st.integers(min_value=1, max_value=4),
+    num_out = st.integers(min_value=1, max_value=4),
+    num_samples = st.integers(min_value=1, max_value=32),
+)
+def test_filtersum_dynamic_no_output_with_increasing_delay(num_in, num_out, num_samples):
+    ir_len = num_samples + 1
+    rng = np.random.default_rng()
+    
+    ir = np.zeros((num_in,num_out, ir_len))
+    filt_dyn = fc.FilterSumDynamic(ir)
+
+    out = np.zeros((num_out, num_samples))
+
+    sig = rng.normal(0, 1, size=(num_in, num_samples))
+    for i in range(num_samples):
+        ir = np.zeros((num_in, num_out, ir_len))
+        ir[:,:,i+1:] = 1
+        filt_dyn.update_ir(ir)
+        out[:,i:i+1] = filt_dyn.process(sig[:,i:i+1])
+
+    assert np.allclose(np.sum(np.abs(out)), 0)
+
+
+
 
 @hyp.settings(deadline=None)
 @hyp.given(
     ir_len = st.integers(min_value=1, max_value=8),
     num_in = st.integers(min_value=1, max_value=4),
     num_out = st.integers(min_value=1, max_value=4),
-    num_samples = st.integers(min_value=1, max_value=32),
-    num_irs = st.integers(min_value=1, max_value=4),
+    num_samples = st.integers(min_value=2, max_value=32),
 )
-def test_filtersum_dynamic_piecewise_static_ir_equals_normal_filtersum(ir_len, num_in, num_out, num_samples, num_irs):
+def test_filtersum_dynamic_piecewise_static_ir_equals_normal_filtersum(ir_len, num_in, num_out, num_samples):
     rng = np.random.default_rng()
-    ir = rng.normal(0, 1, size=(num_in,num_out,ir_len))
+    ir1 = rng.normal(0, 1, size=(num_in,num_out,ir_len))
+    ir2 = rng.normal(0, 1, size=(num_in,num_out,ir_len))
+    change_idx = num_samples // 2
 
-    filt_dyn = fc.FilterSumDynamic(ir)
-    filt = fc.create_filter(ir)
+    filt_dyn = fc.FilterSumDynamic(ir1)
+    filt1 = fc.create_filter(ir1)
+    filt2 = fc.create_filter(ir2)
 
     out = np.zeros((num_out, num_samples))
     out_dyn = np.zeros((num_out, num_samples))
 
     sig = rng.normal(0, 1, size=(num_in, num_samples))
-    for i in range(num_samples):
-        out[:,i:i+1] = filt.process(sig[:,i:i+1])
-        out_dyn[:,i:i+1] = filt_dyn.process(sig[:,i:i+1])
-    # for i in range(num_blocks):
-    #     out[:, i * ir_len : (i + 1) * ir_len] = filt.process(
-    #         sig[:, i * ir_len : (i + 1) * ir_len]
-    #     )
-    #     out_dyn[:, i * ir_len : (i + 1) * ir_len] = filt_dyn.process(
-    #         sig[:, i * ir_len : (i + 1) * ir_len]
-    #     )
+    out[:,:change_idx] = filt1.process(sig[:,:change_idx])
+    filt2.process(sig[:,:change_idx])
+    out[:,change_idx:] = filt2.process(sig[:,change_idx:])
 
-    assert False #it is not implemented yet
+    for i in range(num_samples):
+        out_dyn[:,i:i+1] = filt_dyn.process(sig[:,i:i+1])
+        if i == change_idx-1:
+            filt_dyn.update_ir(ir2)
+
     assert np.allclose(out, out_dyn)
 
 
